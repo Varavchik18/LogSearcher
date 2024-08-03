ed5da0bc5930fc43f452e915e3fc9699678f4f2b
DESKTOP-AI3KLJL\Alex Colba <pashavarava1806@gmail.com>
2024-08-03 13:51:31 +0300

    Added web integrations and increased size of log file to test

--- LogSearcher.Tests/obj/Debug/net8.0/LogSearcher.Tests.AssemblyInfo.cs
+++ LogSearcher.Tests/obj/Debug/net8.0/LogSearcher.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogSearcher.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c80ae03013ac68210314a2f6e27f5294cb2a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogSearcher.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogSearcher.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LogSearcher.Web/Controllers/LogSearchController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class LogSearchController : Controller
+{
+    private readonly LogSearcherService _logSearcherService;
+
+    public LogSearchController()
+    {
+        _logSearcherService = new LogSearcherService();
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Search(IFormFile fileUpload, string searchPattern)
+    {
+        if (fileUpload == null || string.IsNullOrWhiteSpace(searchPattern))
+        {
+            ViewBag.Error = "File and search pattern cannot be empty.";
+            return View("Index");
+        }
+
+        var tempFilePath = Path.GetTempFileName();
+        using (var stream = System.IO.File.Create(tempFilePath))
+        {
+            await fileUpload.CopyToAsync(stream);
+        }
+
+        TempData["UploadedFilePath"] = tempFilePath;
+
+        IEnumerable<string> results = await _logSearcherService.SearchAsync(tempFilePath, searchPattern);
+
+        ViewBag.Results = results;
+        ViewBag.SearchPattern = searchPattern;
+
+        return View("Index");
+    }
+
+
+    public async Task<IActionResult> ViewLog(string filePath, string lineContent)
+    {
+        if (string.IsNullOrWhiteSpace(filePath) || string.IsNullOrWhiteSpace(lineContent))
+        {
+            ViewBag.Error = "File path and line content cannot be empty.";
+            return View("Index");
+        }
+
+        var lines = await System.IO.File.ReadAllLinesAsync(filePath);
+        int lineNumber = Array.IndexOf(lines, lineContent);
+
+        ViewBag.FilePath = filePath;
+        ViewBag.LineContent = lineContent;
+        ViewBag.LineNumber = lineNumber;
+        ViewBag.Lines = lines;
+        return View();
+    }
+
+}
